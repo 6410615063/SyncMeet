cde1c486175da63a1f347bf83ad2dc8df78a0be0
6410615063 <88651097+6410615063@users.noreply.github.com>
2023-11-11 13:11:52 +0000

    fix DAYS_CHOICES ordering

--- mainPage/models.py
+++ mainPage/models.py
@@ -10,18 +10,18 @@ class Activity(models.Model):
     activityId = models.PositiveIntegerField(unique=True)
     
     DAYS_CHOICES = (
+                    ('Sunday','Sunday'),
                     ('Monday','Monday'),
                     ('Tuesday','Tuesday'),
                     ('Wednesday','Wednesday'),
                     ('Thursday','Thursday'),
                     ('Friday','Friday'),
                     ('Saturday','Saturday'),
-                    ('Saturday','Saturday'),
                     )
     start = models.TimeField()
-    start_day = models.CharField(max_length=9, choices=DAYS_CHOICES)
+    start_day = models.CharField(default='Monday', max_length=9, choices=DAYS_CHOICES)
     end = models.TimeField()
-    end_day = models.CharField(max_length=9, choices=DAYS_CHOICES)
+    end_day = models.CharField(default='Monday', max_length=9, choices=DAYS_CHOICES)
 
     def __str__(self):
         return f"From {self.start_day}, {self.start} to {self.end_day}, {self.end}"
