9d0d1cdc4889539801080df8b93770a0662e9687
6410615063 <88651097+6410615063@users.noreply.github.com>
2023-11-11 06:14:19 +0000

    Add placeholder views for Group list and User schedule page

--- mainPage/urls.py
+++ mainPage/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.IndexPage, name='index'),
+    path('', views.IndexPage, name='index'),
     path('login/', views.LoginPage, name='login'),
-    path('signup/',views.SignupPage, name='signup'),
-    path('logout/',views.LogoutPage, name='logout'),
-    path('about', views.about, name="about"),
-    path('/group, views.GroupListPage, name="group"'),
-    path('/schedule, views.UserSchedule, name="schedule_user"'),
+    path('signup/', views.SignupPage, name='signup'),
+    path('logout/', views.LogoutPage, name='logout'),
+    path('about', views.AboutPage, name="about"),
+    path('group', views.GroupListPage, name="group"),
+    path('schedule', views.UserSchedule, name="schedule_user"),
 
 ]
