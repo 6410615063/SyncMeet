2fe4130a69698c786b6e9df5c05ef9241403c7df
6410615063 <88651097+6410615063@users.noreply.github.com>
2023-11-12 14:11:39 +0000

    Add tests for User Schedule page

--- mainPage/tests.py
+++ mainPage/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from mainPage.models import Activity
+from django.db.utils import IntegrityError
+import datetime, time
 # Create your tests here.
 
 
@@ -23,8 +26,9 @@ class UserTest(TestCase):
     
     def test_InValidCreateUser_MissingUsername(self):
         c = Client()
-        response = c.post(reverse('signup'),{'password1':'12345', 'password2':'12345'})
-        self.assertEqual(response.status_code, 200)
+        #response = c.post(reverse('signup'),{'password1':'12345', 'password2':'12345'})
+        #self.assertEqual(response.status_code, 200)
+        self.assertRaises(ValueError, c.post, reverse('signup'),{'password1':'12345', 'password2':'12345'})
 
     def test_InValidCreateUser_DuplicateUsername(self):
         c = Client()
@@ -47,4 +51,32 @@ class UserTest(TestCase):
         response = c.post(reverse('logout'))
         self.assertEqual(response.status_code, 302)
     
+    def test_UserSchedule_notLogin(self):
+        c = Client()
+        #response = c.get(reverse('schedule_user'))
+        #self.assertEqual(response.status_code, 200)
+        self.assertRaises(TypeError, c.get, reverse('schedule_user'))
 
+    def test_UserSchedule_empty(self):
+        c = Client()
+        c.post(reverse('login'),{'username':'test1','pass':'12345'})
+        response = c.get(reverse('schedule_user'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_UserSchedule_not_empty(self):
+        c = Client()
+        user = User.objects.create_user(username='test6', password='12345')
+        c.post(reverse('login'),{'username':'test6','pass':'12345'})
+        Activity.objects.create(user=user, activityId=1, start=datetime.datetime(2020, 5, 5, 10), start_day='Monday', end=datetime.datetime(2020, 5, 6, 10), end_day='Tuesday')
+        response = c.get(reverse('schedule_user'))
+        self.assertEqual(response.status_code, 200)
+        
+    
+    def test_Activity_duplicate_activityId(self):
+        c = Client()
+        user = User.objects.create_user(username='test6', password='12345')
+        c.post(reverse('login'),{'username':'test6','pass':'12345'})
+        Activity.objects.create(user=user, activityId=1, start=datetime.datetime(2020, 5, 5, 10), start_day='Monday', end=datetime.datetime(2020, 5, 6, 10), end_day='Tuesday')
+        #Activity.objects.create(user=user, activityId=1, start=datetime.datetime(2020, 5, 5, 10), start_day='Monday', end=datetime.datetime(2020, 5, 6, 10), end_day='Tuesday')
+        self.assertRaises(IntegrityError, Activity.objects.create, user=user, activityId=1, start=datetime.datetime(2020, 5, 5, 10), start_day='Monday', end=datetime.datetime(2020, 5, 6, 10), end_day='Tuesday')
+        
