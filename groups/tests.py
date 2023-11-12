4accd1bb053c838e2d5f4e895b5865480481e279
6410615063 <88651097+6410615063@users.noreply.github.com>
2023-11-12 15:31:19 +0000

    Remove tests for Group page

--- groups/tests.py
+++ groups/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from user.models import UserInfo
+from django.template.exceptions import TemplateDoesNotExist
 
+from django.db.utils import IntegrityError
 # Create your tests here.
