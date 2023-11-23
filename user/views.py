5c42f881ea383c282fbed770d25df9befebbf52c
6410685058 <88650864+6410685058@users.noreply.github.com>
2023-11-23 15:30:47 +0000

    delete friend

--- user/views.py
+++ user/views.py
@@ -150,3 +150,35 @@ def add_friend(request):
         context['message'] = 'Invalid request method!'
 
     return render(request, 'user/friendlist.html', context)
+
+
+def delete_friend(request):
+
+    user = User.objects.get(username=request.user.username)
+    user_info = UserInfo.objects.get(user_id=user)
+    context = {'userInfo': user_info}
+
+    if request.method == 'POST':
+        user_account_UID = request.POST.get(
+            'user_account_UID')  # เลข account_UID ของผู้ใช้
+        # เลข account_UID ของเพื่อนที่ต้องการเพิ่ม
+        friend_account_UID = request.POST.get('friend_account_UID')
+
+        userInfo = UserInfo.objects.get(account_UID=user_account_UID)
+        all_friend = Friend.objects.filter(user_id=userInfo, status=True)
+
+        # เพิ่มข้อมูลเพิ่มเติมใน context
+        context['all_friend'] = all_friend
+
+        user_info = UserInfo.objects.get(account_UID=user_account_UID)
+        friend_info = UserInfo.objects.get(account_UID=friend_account_UID)
+
+        friend = get_object_or_404(
+            Friend, user_id=user_info, friend_id=friend_info)
+        friend.delete()
+
+        friend_re = get_object_or_404(
+            Friend, user_id=friend_info, friend_id=user_info)
+        friend_re.delete()
+
+    return render(request, 'user/friendlist.html', context)
